5f00fe8d127a208bfbba9cbf447da91c76b6ad40
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2025-08-14 18:05:42 +0100

    Update notices.sql

--- notices.sql
+++ notices.sql
@@ -1,10 +1,11 @@
 
 CREATE TABLE IF NOT EXISTS `notices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) NOT NULL,
-  `title` varchar(50) NOT NULL,
+  `citizenid` varchar(64) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `created_at` datetime NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
